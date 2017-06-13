18e1f1f20ab2959a21107eb3bca400cd1011c353
Anurag Singh <contact@anuragsingh.me>
2017-06-13 12:06:18 +0530

    Create header.php

--- header.php
+++ header.php
@@ -4,14 +4,9 @@
 <head>
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <title><?php echo get_bloginfo('name'); ?></title>
     <meta name="description" content="">
     <meta name="author" content="">
-    <link rel="icon" href="assets/images/favicon.ico">
-    <title><?php echo get_bloginfo('name'); ?></title>
-    <!-- Bootstrap core CSS -->
-    <!-- <link href="assets/css/bootstrap.min.css" rel="stylesheet"> -->
-    <!-- Custom styles for this template -->
-    <!-- <link href="assets/css/carousel.css" rel="stylesheet"> -->
     <?php wp_head(); ?>
 </head>
 
