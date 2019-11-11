3548aebba03b06aab070efb944671d64be5d44f0
Anurag Singh <developer.anuragsingh@outlook.com>
2019-11-11 12:00:49 +0530

    Add a file in library for WP menu items loop

---
+++ lib/wp-menu-items.php
@@ -0,0 +1,21 @@
+<?php
+$menu_name = 'Primary';
+$primary_menu_item_arr = wp_get_nav_menu_items($menu_name, $args = array());
+
+if (!empty($primary_menu_item_arr)) :
+    ?>
+    <div class="collapse navbar-collapse" id="wexim">
+        <div class="navbar-nav w-100 alt-font">
+            <?php
+                foreach ($primary_menu_item_arr as $menu_item_obj) :
+                    $item_title = $menu_item_obj->title;
+                    $item_link = $menu_item_obj->url;
+                    echo '<a class="nav-link scroll" href="' . $item_link . '">' . $item_title . '</a>';
+                endforeach;
+                ?>
+            <span class="menu-line"><i class="fa fa-angle-down" aria-hidden="true"></i></span>
+        </div>
+    </div>
+<?php
+endif;
+?>
