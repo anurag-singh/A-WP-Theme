d197150b33b58d7185628485380f3a34cad4aed2
Anurag Singh <developer.anuragsingh@outlook.com>
2019-11-14 14:06:19 +0530

    add function for maintenance mode

--- inc/as-debugging-functions.php
+++ inc/as-debugging-functions.php
@@ -1,6 +1,17 @@
 <?php
 add_filter('show_admin_bar', '__return_false');     // Hide admin bar from frontend
 
+
+
+// Activate WordPress Maintenance Mode
+// add_action('get_header', 'wp_maintenance_mode');
+function wp_maintenance_mode()
+{
+	if (!current_user_can('edit_themes') || !is_user_logged_in()) {
+		wp_die('<h1>Under Maintenance</h1><br />Website under planned maintenance. Please check back later.');
+	}
+}
+
 // Function to print array value along with array holding var name
 function as_debug($arr)
 {
